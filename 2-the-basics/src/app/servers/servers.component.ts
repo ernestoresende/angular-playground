b0ba436441316cd2c169437caba5f159a80d6429
Ernesto Resende <ernestoresende@hotmail.com>
2020-07-20 15:07:49 -0300

    21. Two-Way Databinding

--- 2-the-basics/src/app/servers/servers.component.ts
+++ 2-the-basics/src/app/servers/servers.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = true;
+  serverCreationStatus = 'No server was crated!'
+  serverName = 'Test Server'
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  } // Default method from TypeScript, first to be loaded.
 
-  ngOnInit(): void {
-  }
+  ngOnInit() {} //  lifecycle hook that is called after Angular has initialized all data-bound properties of a directive.
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!'
+  } // Method that is called when the "Add Server button is clicked"
+
+  onUpdateServerName(event: Event) { 
+    this.serverName = (<HTMLInputElement>event.target).value
+  } // Method that listens for the event target on the form for changes in the input.
 }
